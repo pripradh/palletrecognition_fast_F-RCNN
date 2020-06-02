68cb75b28389af20eeb616b2b3a760d7a5609345
pripradh <pritam.pradhan06@gmail.com>
2020-06-02 16:34:18 -0700

    First Commit

--- keras_frcnn/simple_parser.py
+++ keras_frcnn/simple_parser.py
@@ -18,6 +18,7 @@ def get_data(input_path):
 		for line in f:
 			line_split = line.strip().split(',')
 			(filename,x1,y1,x2,y2,class_name) = line_split
+			
 
 			if class_name not in classes_count:
 				classes_count[class_name] = 1
@@ -33,7 +34,7 @@ def get_data(input_path):
 			if filename not in all_imgs:
 				all_imgs[filename] = {}
 				
-				img = cv2.imread(filename)
+				img = cv2.imread(filename)				
 				(rows,cols) = img.shape[:2]
 				all_imgs[filename]['filepath'] = filename
 				all_imgs[filename]['width'] = cols
@@ -44,7 +45,7 @@ def get_data(input_path):
 				else:
 					all_imgs[filename]['imageset'] = 'test'
 
-			all_imgs[filename]['bboxes'].append({'class': class_name, 'x1': int(x1), 'x2': int(x2), 'y1': int(y1), 'y2': int(y2)})
+			all_imgs[filename]['bboxes'].append({'class': class_name, 'x1': (x1), 'x2': (x2), 'y1': (y1), 'y2': (y2)})
 
 
 		all_data = []
